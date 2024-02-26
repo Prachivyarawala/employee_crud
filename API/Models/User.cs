14466c1a4c5a854e238b8100a509341bc8328a7a
Prachivyarawala <vyarawalaprachi1@gmail.com>
2024-02-26 14:06:50 +0530

    models and repositories

---
+++ API/Models/User.cs
@@ -0,0 +1,25 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+public class User
+{
+    public int c_userid { get; set; }
+
+    [Required(ErrorMessage = "Username is required.")]
+    [StringLength(255, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 255 characters.")]
+    public string c_username { get; set; }
+
+    [Required(ErrorMessage = "Email address is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public string c_useremail { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [StringLength(255, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters.")]
+    public string c_userpassword { get; set; }
+
+    [Compare("c_userpassword", ErrorMessage = "Passwords do not match.")]
+    public string confirmPassword { get; set; }
+}
+}
