0c8776520618894aefd67b7bbd1b9efe119d3d13
“bandhankalkani” <bkalkani710@rku.ac.in>
2024-02-27 11:45:46 +0530

    employeecontyroller in mvc

--- MVC/Controllers/EmployeeController.cs
+++ MVC/Controllers/EmployeeController.cs
@@ -5,34 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using API.Repositories;
-using API.Models;
-
 
 namespace MVC.Controllers
 {
-    // [Route("[controller]")]
+    [Route("[controller]")]
     public class EmployeeController : Controller
     {
         private readonly ILogger<EmployeeController> _logger;
-        private readonly IEmployeeRepositories _emprepo;
 
-        public EmployeeController(ILogger<EmployeeController> logger,IEmployeeRepositories emprepo)
+        public EmployeeController(ILogger<EmployeeController> logger)
         {
             _logger = logger;
-            _emprepo = emprepo;
         }
 
         public IActionResult Index()
         {
-            var emp = _emprepo.FetchoneEmployee();
-            return View(emp);
-        }
-
-        public IActionResult delete(int id)
-        {
-            _emprepo.DeletetEmployee(id);
-            return RedirectToAction("Index");
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
