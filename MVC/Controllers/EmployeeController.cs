d38ddd6df30572ce464331a10f14ac373e6da691
“ishamanore” <ishamanore@gmail.com>
2024-02-27 12:04:03 +0530

    updateEmplolyee Controller

--- MVC/Controllers/EmployeeController.cs
+++ MVC/Controllers/EmployeeController.cs
@@ -39,6 +39,23 @@ namespace MVC.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public IActionResult UpdateEmployee(int id)
+        {
+            string username = HttpContext.Session.GetString("username");
+            if (username == null)
+            {
+                ViewBag.IsAuthenticated = false;
+                return RedirectToAction("Login", "User");
+            }
+            ViewBag.IsAuthenticated = true;
+            var department = _emprepo.GetAllDepartments();
+            var emp = _emprepo.FetchoneEmployee();
+            ViewBag.department = new SelectList(department, "c_deptid", "c_deptname",emp.c_deptname.c_deptid);
+
+            return View(emp);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
