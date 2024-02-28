cfb1b116868101bd5a185370733973094757b5e1
PrachiVyarawala <vyarawalaprachi1@gmail.com>
2024-02-28 16:05:19 +0530

    employeekedio

--- MVC/Controllers/KendoEmployeeMVCController.cs
+++ MVC/Controllers/KendoEmployeeMVCController.cs
@@ -16,16 +16,15 @@ namespace MVC.Controllers
     // [Route("[controller]")]
     public class KendoEmployeeMVCController : Controller
     {
-        // private readonly IAdminRepositories _Adminrepo;
+        // private readonly IAdminRepositories _employeeRepositories;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly IEmployeeRepositories _employeeRepositories;
         public KendoEmployeeMVCController(IEmployeeRepositories employeeRepositories, IWebHostEnvironment webHostEnvironment)
         {
-            // _Adminrepo = Adminrepo;
+            // _employeeRepositories = Adminrepo;
             _webHostEnvironment = webHostEnvironment;
             _employeeRepositories = employeeRepositories;
         }
-
         [Produces("application/json")]
         public IActionResult Index()
         {
@@ -37,35 +36,46 @@ namespace MVC.Controllers
             }
             return View();
         }
-
-        // public IActionResult GetEmployee()
-        // {
-        //     Console.WriteLine("controller ... " + HttpContext.Session.GetInt32("userid"));
-        //     var allEmp = _employeeRepositories.FetchoneEmployee();
-        //     return Json(allEmp);
-        // }
-
         public IActionResult GetAlldept()
         {
             var dept = _employeeRepositories.GetAllDepartments();
 
             return Json(dept);
         }
-
-
         public IActionResult getemployee()
         {
             Console.WriteLine("controller ... " + HttpContext.Session.GetInt32("userid"));
 
             var emp = _employeeRepositories.FetchoneEmployee();
-            Console.WriteLine("name" + emp.c_empname);
-            Console.WriteLine("gender : "+ emp.c_enpgender);
-            Console.WriteLine("image : "+ emp.c_image);
             return Json(emp);
+        }
+        public IActionResult Update(int id)
+        {
+            Console.WriteLine("controller ... " + HttpContext.Session.GetInt32("userid"));
+            Employee employee = _employeeRepositories.FetchoneEmployee();
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            var dept = _employeeRepositories.GetAllDepartments();
+            ViewBag.dept = new SelectList(dept, "c_dept_id", "c_deptname");
+            return View(employee);
         }
 
 
-
+        [HttpPost]
+        public IActionResult Update(Employee employee)
+        {
+            Console.WriteLine("call : " + employee.c_dept_id);
+            if (_employeeRepositories.UpdateEmployee(employee))
+            {
+                return Json(new { success = true, message = "Successfully Added", newEmployeeId = employee.c_empid });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Not Inserted!!" });
+            }
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
