e71034c00a631c5d949b03b9685ffbc1f55babe7
“ishamanore” <ishamanore@gmail.com>
2024-03-04 09:51:15 +0530

    admin

--- MVC/Controllers/KendoCompoAdminController.cs
+++ MVC/Controllers/KendoCompoAdminController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using API.Repositories;
 
 namespace MVC.Controllers
 {
@@ -12,10 +13,12 @@ namespace MVC.Controllers
     public class KendoCompoAdminController : Controller
     {
         private readonly ILogger<KendoCompoAdminController> _logger;
+        private readonly IAdminRepositories _Adminrepo;
 
-        public KendoCompoAdminController(ILogger<KendoCompoAdminController> logger)
+        public KendoCompoAdminController(ILogger<KendoCompoAdminController> logger,  IAdminRepositories Adminrepo)
         {
             _logger = logger;
+            _Adminrepo = Adminrepo;
         }
 
        [Produces("application/json")]
@@ -26,7 +29,20 @@ namespace MVC.Controllers
             {
                 return RedirectToAction("Login", "User");
             }
-            return View();
+            var employee = _Adminrepo.getAllEmployee();
+            return View(employee);
+        }
+
+        public IActionResult GetAlldept()
+        {
+            var dept = _Adminrepo.GetAllDepartments();
+
+            return Json(dept);
+        }
+        public IActionResult getAllEmployee()
+        {
+            var employee = _Adminrepo.getAllEmployee();
+            return Json(employee);
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
